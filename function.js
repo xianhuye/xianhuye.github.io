1f1b04247190c3b4f182b3e1971fd30d644d0319
Lam Sing Yip <lamye1992@gmail.com>
2020-05-13 16:17:09 -0700

    update

--- function.js
+++ function.js
@@ -5,3 +5,35 @@ function resumeBt(){
   document.getElementById('resumeP').style.display = 'block';
   document.getElementsByTagName('body')[0].style.backgroundColor = '#dedede';
 };
+
+function tinyBt(){
+  document.getElementById('interiorMenu').style.display = 'none';
+  document.getElementById('tinyHouse').style.display = 'block';
+  document.getElementById('codesLab').style.display = 'none';
+  document.getElementById('aNh').style.display = 'none';
+  document.getElementById('new').style.display = 'none';
+}
+
+function codeBt(){
+  document.getElementById('interiorMenu').style.display = 'none';
+  document.getElementById('tinyHouse').style.display = 'none';
+  document.getElementById('codesLab').style.display = 'block';
+  document.getElementById('aNh').style.display = 'none';
+  document.getElementById('new').style.display = 'none';
+}
+
+function aNhBt(){
+  document.getElementById('interiorMenu').style.display = 'none';
+  document.getElementById('tinyHouse').style.display = 'none';
+  document.getElementById('codesLab').style.display = 'none';
+  document.getElementById('aNh').style.display = 'block';
+  document.getElementById('new').style.display = 'none';
+}
+
+function newBt(){
+  document.getElementById('interiorMenu').style.display = 'none';
+  document.getElementById('tinyHouse').style.display = 'none';
+  document.getElementById('codesLab').style.display = 'none';
+  document.getElementById('aNh').style.display = 'none';
+  document.getElementById('new').style.display = 'block';
+}
