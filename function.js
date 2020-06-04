71f71bed8077334802d4c08c83d7ddec41b48970
Lam Sing Yip <lamye1992@gmail.com>
2020-06-03 19:37:04 -0700

    photo page added

--- function.js
+++ function.js
@@ -37,3 +37,163 @@ function newBt(){
   document.getElementById('aNh').style.display = 'none';
   document.getElementById('new').style.display = 'block';
 }
+
+
+//photography page ---------------------------------------------------------------------
+function nikonPic(){
+  document.getElementById('photoP').style.display = 'none';
+  document.getElementById('nikonP').style.display = 'block';
+  document.getElementById('phonesP').style.display = 'none';
+  document.getElementById('goproP').style.display = 'none';
+}
+
+function chengduB(){
+  document.getElementById('chengduT').style.color = 'black';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'block';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'none';
+}
+function beijingB(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = 'black';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'block';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'none';
+}
+function wwhB(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = 'black';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'block';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'none';
+}
+function tokyoB(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = 'black';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'block';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'none';
+}
+function thailandB(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = 'black';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'block';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'none';
+}
+function photo51B(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = 'black';
+  document.getElementById('dailyT').style.color = '#a3a5a4';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'block';
+  document.getElementById('daily').style.display = 'none';
+}
+function dailyB(){
+  document.getElementById('chengduT').style.color = '#a3a5a4';
+  document.getElementById('beijingT').style.color = '#a3a5a4';
+  document.getElementById('wwhT').style.color = '#a3a5a4';
+  document.getElementById('tokyoT').style.color = '#a3a5a4';
+  document.getElementById('thailandT').style.color = '#a3a5a4';
+  document.getElementById('photo51T').style.color = '#a3a5a4';
+  document.getElementById('dailyT').style.color = 'black';
+  document.getElementById('chengdu').style.display = 'none';
+  document.getElementById('beijing').style.display = 'none';
+  document.getElementById('wwh').style.display = 'none';
+  document.getElementById('tokyo').style.display = 'none';
+  document.getElementById('thailand').style.display = 'none';
+  document.getElementById('photo51').style.display = 'none';
+  document.getElementById('daily').style.display = 'block';
+}
+
+function phonesP(){
+  document.getElementById('photoP').style.display = 'none';
+  document.getElementById('nikonP').style.display = 'none';
+  document.getElementById('phonesP').style.display = 'block';
+  document.getElementById('goproP').style.display = 'none';
+}
+function iphoneB(){
+  document.getElementById('iphoneT').style.color = 'black';
+  document.getElementById('samsungT').style.color = '#a3a5a4';
+  document.getElementById('iphone').style.display = 'block';
+  document.getElementById('samsung').style.display = 'none';
+}
+function samsungB(){
+  document.getElementById('iphoneT').style.color = '#a3a5a4';
+  document.getElementById('samsungT').style.color = 'black';
+  document.getElementById('iphone').style.display = 'none';
+  document.getElementById('samsung').style.display = 'block';
+}
+
+function goproP(){
+  document.getElementById('photoP').style.display = 'none';
+  document.getElementById('nikonP').style.display = 'none';
+  document.getElementById('phonesP').style.display = 'none';
+  document.getElementById('goproP').style.display = 'block';
+}
+function photoB(){
+  document.getElementById('photoT').style.color = 'black';
+  document.getElementById('videoT').style.color = '#a3a5a4';
+  document.getElementById('photo').style.display = 'block';
+  document.getElementById('video').style.display = 'none';
+}
+function videoB(){
+  document.getElementById('photoT').style.color = '#a3a5a4';
+  document.getElementById('videoT').style.color = 'black';
+  document.getElementById('photo').style.display = 'none';
+  document.getElementById('video').style.display = 'block';
+}
